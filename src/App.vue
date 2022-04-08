6dfd891be639411273a9515597a86c9bf9a46e64
Patricia Namoro <namoropatriciamae@gmail.com>
2022-04-08 21:40:16 +0800

    Remove v-bind in App Template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <h1>DC Heroes</h1>
   <ul>
-    <li  v-for="(hero, index) in dcHeroes" v-bind:key="index">{{ index }} : {{hero.name}}</li>
-    <input type="text" v-bind:value="newHero">
-    <button v-bind:disabled="isDisabled">Add Hero</button>
+    <li  v-for="(hero, index) in dcHeroes" :key="index">{{ index }} : {{hero.name}}</li>
+    <input type="text" :value="newHero">
+    <button :disabled="isDisabled">Add Hero</button>
   </ul>
 </template>
 
