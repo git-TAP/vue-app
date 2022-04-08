6b628d987454fcc4739e4bac957b728c6d506bc8
Patricia Namoro <namoropatriciamae@gmail.com>
2022-04-08 16:20:42 +0800

    Count using setInterval function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <h1 v-if="isShowing" v-html="title"></h1>
-  <h2 v-else-if="isShowing == null">This is v-else-if part</h2>
-  <h3 v-else>This is v-else part</h3>
+  <h1  v-text="count"></h1>
+  <!-- <h2 v-else-if="isShowing == null">This is v-else-if part</h2>
+  <h3 v-else>This is v-else part</h3> -->
   
 </template>
 
@@ -12,7 +12,13 @@
       return{
         title:"<h1>This is the Title inside the H1 tag</h1>",
         isShowing: false,
+        count:0,
       }
+    },
+    mounted(){
+      setInterval(() => {
+        this.count++;
+      }, 1000)
     }
   }
 </script>
